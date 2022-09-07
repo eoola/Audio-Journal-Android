558d677646c6c370ec7e7eb41fd24b6616d9c37a
Vansh Patel <vspatel@wpi.edu>
2022-09-07 19:37:13 -0400

    Loading screen + navigation setup

--- app/src/main/java/com/wpi/audiojournal/MainActivity.kt
+++ app/src/main/java/com/wpi/audiojournal/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.wpi.audiojournal.ui.theme.AudioJournalTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +20,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    SetupNavGraph(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AudioJournalTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/wpi/audiojournal/screen/SplashScreen.kt
@@ -0,0 +1,53 @@
+package com.wpi.audiojournal.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.wpi.audiojournal.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController){
+    LaunchedEffect(key1 = true){
+        delay(3000)
+        navController.navigate("home")
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = colorResource(R.color.light_blue))){
+        Column (verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Audio Journal", textAlign = TextAlign.Center, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            Modifier.padding(40.dp)
+            Image(
+                painter = painterResource(id = R.drawable.loading_screen_mic),
+                contentDescription = "Microphone",
+                Modifier.weight(1f),
+                contentScale = ContentScale.FillHeight
+                )
+            CircularProgressIndicator()
+            Text(text = "Made in Cooperation with", textAlign = TextAlign.Center, fontSize = 30.sp)
+            Modifier.padding(40.dp)
+            Image(
+                painter = painterResource(id = R.drawable.loading_screen_wpi_logo),
+                contentDescription = "WPI Logo"
+            )
+        }
+    }
+    
+}
