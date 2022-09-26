e25e17a3d1b5742fb94cb4820baf72b217a55a90
Emmanuel Ola <54866720+eoola@users.noreply.github.com>
2022-09-26 15:46:00 -0400

    Create AudioJournalService.kt

---
+++ app/src/main/java/com/wpi/audiojournal/repositories/AudioJournalService.kt
@@ -0,0 +1,28 @@
+package com.wpi.audiojournal.repositories
+
+import com.wpi.audiojournal.models.CategoriesDTO
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+const val BASE_URL = "https://audiojournal.org/wp-json/swws/v1/"
+
+interface AudioJournalService {
+
+    @GET("categories")
+    fun getCategories(): Call<CategoriesDTO>
+
+    companion object {
+        fun create(): AudioJournalService {
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(AudioJournalService::class.java)
+        }
+    }
+}
