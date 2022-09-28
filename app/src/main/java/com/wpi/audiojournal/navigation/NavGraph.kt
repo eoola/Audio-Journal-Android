a2d11b987d95d8bec2a01a200f64fd64454b2239
Vansh Patel <vspatel@wpi.edu>
2022-09-28 14:31:37 -0400

    creating program schedule screen button layout + individual days' schedule initial layouts (rebasing from main after merge)

--- app/src/main/java/com/wpi/audiojournal/navigation/NavGraph.kt
+++ app/src/main/java/com/wpi/audiojournal/navigation/NavGraph.kt
@@ -2,11 +2,14 @@ package com.wpi.audiojournal.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.wpi.audiojournal.screen.DailySchedule
 import com.wpi.audiojournal.models.MenuItem
 import com.wpi.audiojournal.screen.HomeScreen
-import com.wpi.audiojournal.screen.ProgramCategoriesScreen
+import com.wpi.audiojournal.screen.ProgramSchedule
 import com.wpi.audiojournal.screen.SplashScreen
 import com.wpi.audiojournal.data.DataStuff
 import com.wpi.audiojournal.screen.*
@@ -26,7 +29,23 @@ fun SetupNavGraph(navController: NavHostController, data: ArchiveDataObjects){
                 MenuItem("Favorite Programs"),
                 MenuItem("Program Schedule"),
                 MenuItem("Help")
-            ), navController=navController)
+            ), navController = navController)
+        }
+        composable("Program Schedule"){
+            ProgramSchedule(menuItems = listOf(
+                MenuItem("Sunday"),
+                MenuItem("Monday"),
+                MenuItem("Tuesday"),
+                MenuItem("Wednesday"),
+                MenuItem("Thursday"),
+                MenuItem("Friday"),
+                MenuItem("Saturday")
+            ), navController = navController, title = "Program Schedule")
+        }
+
+        composable("${Screens.programScheduleScreen.route}/{day}", arguments = listOf(navArgument("day") { type = NavType.StringType })){
+            DailySchedule(navController = navController, title = it.arguments?.getString("day"))
+            ), navController = navController)
         }
         composable("Archived Programs"){
             ProgramCategoriesScreen(menuItems = listOf(
