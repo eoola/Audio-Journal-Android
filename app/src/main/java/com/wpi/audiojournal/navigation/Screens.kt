da599631e2086299240b163196f5ab98df49b504
Vansh Patel <vspatel@wpi.edu>
2022-09-28 14:31:37 -0400

    rebasing...

--- app/src/main/java/com/wpi/audiojournal/navigation/Screens.kt
+++ app/src/main/java/com/wpi/audiojournal/navigation/Screens.kt
@@ -4,6 +4,8 @@ sealed class Screens (val route: String){
 
     object homeScreen : Screens("Home")
     object loadingScreen: Screens("loading")
+    object programScheduleScreen: Screens("Program Schedule")
+    object dynamicProgramScheduleView: Screens("Schedule")
     object archivedScreen: Screens("Archived Programs")
     object programOptionScreen: Screens("program-option")
     object programDetailScreen: Screens("program-detail")
