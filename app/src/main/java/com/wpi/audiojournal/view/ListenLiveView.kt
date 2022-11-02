b24877b60cd37bcdb0fb22cede2e38247cbc608f
Vansh Patel <vspatel@wpi.edu>
2022-11-02 18:01:47 -0400

    added networking for listen live after refactor

--- app/src/main/java/com/wpi/audiojournal/view/ListenLiveView.kt
+++ app/src/main/java/com/wpi/audiojournal/view/ListenLiveView.kt
@@ -1,33 +1,42 @@
 package com.wpi.audiojournal.view
 
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.wpi.audiojournal.ui.component.LiveControls
-import com.wpi.audiojournal.ui.component.Player
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.wpi.audiojournal.R
+import com.wpi.audiojournal.ui.component.LiveControls
+import com.wpi.audiojournal.ui.component.Loading
 import com.wpi.audiojournal.ui.component.PageSkeleton
+import com.wpi.audiojournal.ui.component.Player
+import com.wpi.audiojournal.viewmodels.LivestreamViewModel
 
-val uri: Uri = Uri.parse("https://stream-47.zeno.fm/nvbenz3c19duv")
 @Composable
-fun ListenLiveView(title: String) {
+fun ListenLiveView(title: String, viewModel: LivestreamViewModel = viewModel()) {
+    LaunchedEffect(LocalContext.current) {
+        viewModel.loadUri()
+    }
+
     PageSkeleton(header = title) {
-        Image(
-            painter = painterResource(id = R.drawable.loading_screen_mic),
-            contentDescription = "Album Art",
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .padding(50.dp)
-                .fillMaxWidth(0.5F)
-        )
-        Player(uri) {
-            LiveControls(it)
+        Loading(data = viewModel.uri) { uri ->
+            Image(
+                painter = painterResource(id = R.drawable.loading_screen_mic),
+                contentDescription = "Album Art",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .padding(50.dp)
+                    .fillMaxWidth(0.5F)
+            )
+            Player(uri) {
+                LiveControls(it)
+            }
         }
     }
 }
