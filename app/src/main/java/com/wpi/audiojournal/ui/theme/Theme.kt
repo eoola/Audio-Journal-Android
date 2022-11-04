64972be29a33111c00197d5d568015df790a9389
Reily Siegel <mail@reilysiegel.com>
2022-11-04 16:21:16 -0400

    Add selectable color theme.

--- app/src/main/java/com/wpi/audiojournal/ui/theme/Theme.kt
+++ app/src/main/java/com/wpi/audiojournal/ui/theme/Theme.kt
@@ -4,50 +4,72 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 
 
-private val DarkColorPalette = darkColors(
-    primary = Blue,
-    primaryVariant = Purple,
-    secondary = Salmon
+private fun DarkColorPalette(colorScheme: ColorScheme) = darkColors(
+    primary = colorScheme.getRainbowColor(0),
+    primaryVariant = colorScheme.getRainbowColor(1),
+    secondary = colorScheme.getRainbowColor(2)
 )
 
-private val LightColorPalette = lightColors(
-    primary = Blue,
-    primaryVariant = Purple,
-    secondary = Salmon
+private fun LightColorPalette(colorScheme: ColorScheme) = lightColors(
+    primary = colorScheme.getRainbowColor(0),
+    primaryVariant = colorScheme.getRainbowColor(1),
+    secondary = colorScheme.getRainbowColor(2)
 )
 
 data class ColorScheme(val colors: List<Color>, val background: Color, val content: Color, val borders: Color) {
     fun getRainbowColor(index: Int) = colors[index % colors.size]
 }
 
-
-
-val LocalColorScheme = compositionLocalOf {
+val colorSchemes = listOf(
     ColorScheme(
         listOf(Purple, Salmon, Orange, Teal, Yellow, Blue),
         Gray,
         Color.Black,
         Color.Black
+    ),
+    ColorScheme(
+        listOf(Color.White),
+        Blue,
+        Color.Black,
+        Color.Black
+    ),
+    ColorScheme(
+        listOf(Color.White),
+        Teal,
+        Color.Black,
+        Color.Black
     )
-}
+)
+
+fun nextColorScheme (from: ColorScheme) =
+    colorSchemes[colorSchemes.indexOf(from).inc().mod(colorSchemes.size)]
+
+
+val LocalColorScheme = staticCompositionLocalOf { colorSchemes.first() }
 
 @Composable
-fun AudioJournalTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+fun AudioJournalTheme(darkTheme: Boolean = isSystemInDarkTheme(),
+                      content: @Composable ((ColorScheme) -> Unit) -> Unit) {
+    val (colorScheme, setColorScheme) = remember {
+        mutableStateOf(colorSchemes.first())
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    val colors = if (darkTheme) {
+        DarkColorPalette(colorScheme)
+    } else {
+        LightColorPalette(colorScheme)
+    }
+
+    CompositionLocalProvider (LocalColorScheme provides colorScheme) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = { content(setColorScheme) }
+        )
+    }
 }
--- app/src/main/java/com/wpi/audiojournal/view/HomeView.kt
+++ app/src/main/java/com/wpi/audiojournal/view/HomeView.kt
@@ -1,14 +1,28 @@
 package com.wpi.audiojournal.view
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.wpi.audiojournal.models.MenuItem
 import com.wpi.audiojournal.ui.component.Menu
 import com.wpi.audiojournal.ui.component.PageSkeleton
+import com.wpi.audiojournal.ui.theme.Blue
+import com.wpi.audiojournal.ui.theme.ColorScheme
+import com.wpi.audiojournal.ui.theme.LocalColorScheme
+import com.wpi.audiojournal.ui.theme.nextColorScheme
 
 @Composable
-fun HomeView(menuItems: List<MenuItem>, navController: NavController) {
+fun HomeView(
+    menuItems: List<MenuItem>,
+    navController: NavController,
+    setColorScheme: (ColorScheme) -> Unit
+) {
+    val colors = LocalColorScheme.current
     PageSkeleton(header = "Audio Journal") {
         Menu(menuItems = menuItems, navController = navController)
+        Button(onClick = { setColorScheme(nextColorScheme(colors)) }) {
+            Text(text = "Color")
+        }
     }
 }
