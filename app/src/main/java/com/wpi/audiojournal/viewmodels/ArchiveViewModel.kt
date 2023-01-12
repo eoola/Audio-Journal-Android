6a4b41e437e0df60ec05b56f2c6f80d07aeb739a
Reily Siegel <reily@deathfire.netbird.cloud>
2023-01-12 17:20:03 -0500

    Prevent app from crashing on network error.
    This adds an exception handler to network-related coroutines to catch networking
    exceptions and show a toast to the user. All other exceptions are re-thrown.

--- app/src/main/java/com/wpi/audiojournal/viewmodels/ArchiveViewModel.kt
+++ app/src/main/java/com/wpi/audiojournal/viewmodels/ArchiveViewModel.kt
@@ -1,18 +1,20 @@
 package com.wpi.audiojournal.viewmodels
 
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.wpi.audiojournal.models.Category
 import com.wpi.audiojournal.repositories.AudioJournalService
+import com.wpi.audiojournal.repositories.networkHandler
 import kotlinx.coroutines.launch
 
-class ArchiveViewModel: ViewModel() {
+class ArchiveViewModel(application: Application) : AndroidViewModel(application) {
     var categories: List<Category>? by mutableStateOf(null)
 
-    fun loadCategories() = viewModelScope.launch {
+    fun loadCategories() = viewModelScope.launch(networkHandler(getApplication())) {
         categories = AudioJournalService.getCategories().body() ?: listOf()
     }
 }
