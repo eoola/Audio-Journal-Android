9bc7c7484ca342528296f261f3cb53c0ad2b6870
Reily Siegel <mail@reilysiegel.com>
2023-01-24 13:23:52 -0500

    Add integration with Google Assistant for search.
    This commit modifies the search functionality of the app to be compatible with
    Google Assistant. Future commits will extend this integration to also expose
    other app functions, such as listen live or the schedule.

--- app/src/main/java/com/wpi/audiojournal/viewmodels/SearchProgramsViewModel.kt
+++ app/src/main/java/com/wpi/audiojournal/viewmodels/SearchProgramsViewModel.kt
@@ -5,47 +5,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.exoplayer2.util.Log
 import com.wpi.audiojournal.models.Program
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SearchProgramsViewModel(programList: List<Program>): ViewModel() {
-    var state by mutableStateOf(MenuItemSearchState(list =programList))
-
-    var programs = programList
+class SearchProgramsViewModel(): ViewModel() {
+    var text by mutableStateOf("")
+    var programs by mutableStateOf(listOf<Program>())
 
     private var searchJob: Job? = null
 
-    fun onAction(userAction: UserAction){
-        when(userAction){
-            is UserAction.TextFieldInput -> {
-                state = state.copy(searchText = userAction.text)
-                searchJob?.cancel()
-                searchJob = viewModelScope.launch {
-                    delay(500L)
-                    searchProgramsInList(searchQuery = userAction.text)
-                }
-            }
+    fun onAction(text: String){
+        Log.i("tag", text)
+        this.text = text
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            programs = Program.search(text)
         }
-
     }
-
-    private fun searchProgramsInList(
-        searchQuery: String
-    ) {
-        val newList = programs.filter {
-            it.title.contains(searchQuery, ignoreCase = true)
-        }
-        state = state.copy(list = newList)
-    }
-}
-
-data class MenuItemSearchState(
-    var searchText:String="",
-    val list: List<Program>
-)
-
-sealed class UserAction{
-    data class TextFieldInput(val text:String): UserAction()
 }
