2f612fecb01280e20219d8f24a6bdce728ff6cc9
Reily Siegel <mail@reilysiegel.com>
2023-02-15 16:29:06 -0500

    Remove inline funciton.

--- app/src/main/java/com/wpi/audiojournal/ui/component/Helpers.kt
+++ app/src/main/java/com/wpi/audiojournal/ui/component/Helpers.kt
@@ -7,10 +7,10 @@ import androidx.compose.ui.graphics.Color
 import com.wpi.audiojournal.ui.theme.LocalColorScheme
 
 
-inline fun <T> LazyListScope.rainbow(
+fun <T> LazyListScope.rainbow(
     items: List<T>,
-    noinline key: ((item: T) -> Any)? = null,
-    crossinline itemContent: @Composable LazyItemScope.(item: T, color: Color) -> Unit
+    key: ((item: T) -> Any)? = null,
+    itemContent: @Composable LazyItemScope.(item: T, color: Color) -> Unit
 ) = items(items.size, if (key != null) { index: Int -> key(items[index]) } else null) {
     itemContent(items[it], LocalColorScheme.current.getRainbowColor(it))
 }
